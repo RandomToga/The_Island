ed6f9c3988c9dde3143476cdea93fc75023d0c61
RandomToga <maria.09.04.vau@gmail.com>
2025-05-28 19:22:54 +0300

    добавлен класс button

---
+++ VisualNovel/Scenes/MenuScene.cs
@@ -0,0 +1,31 @@
+﻿/*using Microsoft.Xna.Framework.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Island.Scenes
+{
+    public class MenuScene : IScene
+    {
+        public void Load(ContentManager content)
+        {
+            Console.WriteLine("Инициализация меню...");
+            // Здесь можно загрузить ресурсы для меню, например, фоновое изображение, кнопки и т.д.
+        }
+        public void Update(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            // Здесь можно обработать ввод пользователя для навигации по меню
+            // Например, проверка нажатия клавиш для выбора опций
+        }
+        public void Draw(Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch)
+        {
+            // Здесь можно отрисовать элементы меню, такие как фон, кнопки и т.д.
+            spriteBatch.Begin();
+            // spriteBatch.Draw(backgroundTexture, Vector2.Zero, Color.White); // Пример отрисовки фона
+            spriteBatch.End();
+        }
+
+    }
+}*/
